bcdba89967ec4aef65e0e8432a1f46aaac509d35
csamuro <magomedov.m25@wb.ru>
2023-02-20 00:54:20 +0600

    for stage

--- cmd/tg_bot/main.go
+++ cmd/tg_bot/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/3Danger/telegram_bot/internal/config"
+	tg_bot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/rs/zerolog"
 	"github.com/vrischmann/envconfig"
 	"os"
@@ -12,7 +14,22 @@ func main() {
 	if err != nil {
 		logger.Fatal().Err(err).Msg("couldn't init")
 	}
-	_ = conf
+	api, err := tg_bot.NewBotAPI(conf.Telegram.Token)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("couldn't create tg api")
+	}
+	api.Debug = true
+
+	u := tg_bot.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := api.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		fmt.Printf("%+v", update)
+	}
 }
 
 func initiate() (*config.Config, zerolog.Logger, error) {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/3Danger/telegram_bot
 go 1.20
 
 require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/rs/zerolog v1.29.0 // indirect
