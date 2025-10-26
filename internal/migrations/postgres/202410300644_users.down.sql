4873b0cf9bace51f0939806767ff144728d5943d
Rasul <magomedrasul.magomedov@vitech.team>
2025-10-26 20:50:21 +0100

    --- DROP ---

--- internal/migrations/postgres/202410300644_users.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE users;
-
-DROP TYPE USER_TYPE;
--- internal/repo/interface.go
+++
@@ -1,9 +0,0 @@
-package repo
-
-import "context"
-
-type Repo[T any] interface {
-	Get(ctx context.Context, userID int) (T, error)
-	Set(ctx context.Context, userID int, data T) error
-	Delete(ctx context.Context, userID int) error
-}
